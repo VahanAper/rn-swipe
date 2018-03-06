c6da6bd666e8176c43501f7adca8397bd319514c
Vahan Asryan <v.asryan@apollobytes.com>
2018-03-06 10:57:27 +0400

    force swipe the card to left and right

--- src/Deck.js
+++ src/Deck.js
@@ -7,6 +7,10 @@ import {
 } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPTE_OUT_DURATION = 250;
+const LEFT = "LEFT";
+const RIGHT = "RIGHT";
 
 class Deck extends React.Component {
     constructor(props) {
@@ -22,10 +26,35 @@ class Deck extends React.Component {
                     y: gesture.dy,
                 });
             },
-            onPanResponderRelease: () => {
-                this.resetPosition()
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipeCardTo(RIGHT);
+                } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                    this.forceSwipeCardTo(LEFT);
+                } else {
+                    this.resetPosition();
+                }
             },
         });
+    }
+    
+    forceSwipeCardTo = (direction) => {
+        const x = direction === RIGHT 
+            ? SCREEN_WIDTH 
+            : direction === LEFT 
+                ? -SCREEN_WIDTH
+                : 0;
+        
+        Animated.timing(
+            this.position,
+            {
+                toValue: {
+                    x,
+                    y: 0,
+                },
+                duration: SWIPTE_OUT_DURATION, // ms
+            },
+        ).start();
     }
     
     resetPosition = () => {
